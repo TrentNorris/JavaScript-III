d1e8adf28d45f1a2f83facba98eb7fc5bd4fe8d9
Trent Norris <trent.norris018@gmail.com>
2019-12-06 02:33:10 -0800

    4th question complete, principle 4 complete

--- assignments/this.js
+++ assignments/this.js
@@ -4,7 +4,7 @@
 * 1. Window Binding, using this to select the window/console object on a global scope.
 * 2. Implicit Binding, using this in a object's method, accessing data from said method.
 * 3. New Binding, using this inside a function, creating a constructor, with an inner function that is excuted through the variable we set.
-* 4. 
+* 4. Explicit Binding, is when a call or apply method is used to explicitly set this.
 *
 * write out a code example of each explanation above
 */
@@ -46,3 +46,18 @@ trentsRoom.cleanRoom();
 // Principle 4
 
 // code example for Explicit Binding
+function createOffice(office_name) {
+    this.office_name = office_name;
+    this.size = 'large';
+    this.computers = 2;
+    this.lockOffice = function() {
+        console.log('Locked ' + office_name + '\'s office.');
+    };
+}
+let trentsOffice = new createOffice('Trent');
+let joshsOffice = new createOffice('Josh');
+trentsOffice.lockOffice.call(joshsOffice);
+joshsOffice.lockOffice.apply(trentsOffice);
+
+trentsOffice.lockOffice();
+joshsOffice.lockOffice();
